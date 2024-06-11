c2ca26e5a18668fb85bb86bb3ce072a3d966163f
jb17899 <bksharma1224@gmail.com>
2024-06-11 22:17:56 +0530

    modified

--- index.js
+++ index.js
@@ -1,144 +1,185 @@
 import express from "express"
-import pg from "pg"
+import supabase from "./supabase.js";
+import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import {Strategy} from "passport-local";
+import dotenv from "dotenv";
+dotenv.config();
 const months = [
     "January", "February", "March", "April", "May", "June", 
     "July", "August", "September", "October", "November", "December"
   ];
 const app = express();
 const port = 3012;
-const db = new pg.Client({
-    user:"postgres",
-    host:"localhost",
-    database:"postgres",
-    password:"admin",
-    port:5432
-});
+const saltRounds = 10;
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
-db.connect();
-const user={
-    name:"",
-    error:false,
-    err_string:"",
-    err:function(){
-        if(user.error == false){
-            return false;
-        }
-        else{
-            return this.err_string;
-        }
-    }
-}
-const login={
-    bool_login:false,
-    login_name:"",
-    login:()=>{
-        if(!login.bool_login){
-            return false;     
-        }
-        else{
-            return this.login_name;
-        }
-    }
-}
+app.use(session({
+    secret:process.env.SECRET_WORD,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:1000*60*60*24,
+      }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.get("/",(req,res)=>{
-    res.render("index.ejs",{login:login.bool_login});
+  var loggedIn =false;
+    if(req.user){
+        loggedIn =true;
+  }
+res.render("index.ejs",{
+    login:loggedIn
+});
 });
 app.get("/login",(req,res)=>{
+    var loggedIn =false;
+    if(req.user)loggedIn = true;
     res.render("login.ejs",{
-        login:login.bool_login
+        login:loggedIn
     });
 });
 app.get("/signsup",(req,res)=>{
     res.render("sign_up.ejs",{
-        error:user.err(),
-        login:login.bool_login
+     login:(req.user)?true:false   
     });
 })
-app.post("/signin",async(req,res)=>{
-   console.log(req.body);
-   var username_email = req.body.username;
-   var password = req.body.password;
-   const results = await db.query("SELECT name from users where name = $1 and password = $2 or email = $1 and password  = $2",[username_email,password]);
-   console.log(results.rows);
-   if(results.rows[0]){
-    user.name = results.rows[0].name;
-login.bool_login=true;
-setTimeout(()=>{ 
-    res.redirect("/");
-},300);}
-else{
-    res.redirect("/login");
-}
-});
+app.post("/signin",passport.authenticate("local",{
+    successRedirect:"/",
+    failureRedirect:'/login'
+}));
+
+passport.use('local',
+    new Strategy(async function (username,password,cb){
+        const {data,error} = await supabase
+        .from('userm')
+        .select()
+        .eq('email',username)
+        bcrypt.compare(password,data[0].password,(err,result)=>{
+         if(err) throw err;
+         if(result){
+             return cb(null,data[0]);
+            }
+            else{
+             return cb("Password not matched");
+            }
+        })
+    })
+)
 app.post("/signsin",async(req,res)=>{
-    console.log(req.body);
     const username = req.body.username;
     const email = req.body.email;
-    const phone = req.body.phone;
+    const phone_number = req.body.phone.toString();
     const password = req.body.password;
     try{
-    const results  = await db.query("INSERT INTO users(name,email,phone_number,password) VALUES($1,$2,$3,$4)",[username,email,phone,password]);
-    user.name = username;
-    login.bool_login=true;
-    res.redirect("/order_now");}
-    catch{
-        user.error=true;
-        user.err_string="write truthfully please!";
-        res.redirect('/signsup');
+    bcrypt.hash(password,saltRounds,async(err,hash)=>{
+        if (err) throw err;
+        const {data,error} = await supabase
+        .from('userm')
+        .insert([
+            {name:username,email:email,phone_number:phone_number,password:hash},
+        ])
+        .select()
+        if(error){
+            console.log(error);
+            res.redirect('/signsup');
+        }
+        else{
+        req.login(data[0],(err)=>{
+            if(err) throw err;
+            res.redirect('/');
+        })}
+    })
     }
-
+    catch(errs){
+        console.log(errs);
+    }
 });
 app.post("/order_now",async(req,res)=>{
-  console.log(req.body);
   const restaurent_name = req.body.restaurent;
-  var item = req.body.food_item;
-  item = item[0].toUpperCase()+item.slice(1);
+  const item_name = req.body.food_item;
   const address = req.body.address;
-  const results = await db.query("select id,price from restaurents where food=$1 and name = $2",[item,restaurent_name]);
-  var food_id = results.rows[0].id;
-  var price = results.rows[0].price;
   var date = new Date();
   var day = date.getDate();
   var month = date.getMonth();
   var year = date.getFullYear();
   var dates =months[month]+' '+day+','+year;
-  console.log(dates);
-  const ids = await db.query("SELECT id from users where name = $1",[user.name]);
-  const result = await db.query("INSERT INTO orders(user_id,restaurent_id,address,price,times) VALUES($1,$2,$3,$4,$5)",[ids.rows[0].id,food_id,address,price,dates]);
-  res.render("order_been_accept.ejs");
+  const {data,error} = await supabase
+  .from('restaurents')
+  .select()
+  .eq('name',restaurent_name)
+  .eq('food',item_name)
+  try{
+    if(data[0].availability==true){
+        const {datas,error} = await supabase
+        .from('orders')
+        .insert([
+            {user_id:req.user.id,restaurent_id:data[0].id,address:address,price:data[0].price,time:dates}
+        ])
+        .select()
+        if(error){
+            console.log(error);
+            res.redirect('/');
+        }
+        else{
+        res.render("order_been_accept.ejs");
+        }
+    }
+  }
+  catch(err){
+    if(err) console.log(err);
+    res.redirect('/');
+  }
   }
 );
 app.get("/order_now",(req,res)=>{
+    if(req.isAuthenticated()){
     res.render("order_now.ejs",{
-        login:login.bool_login
-    });
+        login:(req.user)?true:false
+    });}
+    else{
+        res.redirect('/login');
+    }
 })
 app.get("/order",async(req,res)=>{
-    const results = await db.query("select * from orders");
-    const array = results.rows;
-    console.log(array);
+    var array=[];
+    if(req.user){
+    const {data,error} = await supabase
+    .from('orders')
+    .select()
+    .eq('user_id',req.user.id)
+    array=data;
+    }
     res.render("orders.ejs",{
-        items:results.rows,
-        login:login.bool_login
+        items:(req.user)?array:false,
+        login:(req.user)?true:false
     });
 });
 app.get("/faq",(req,res)=>{
     res.render("faq.ejs",{
-        login:login.bool_login
+        login:(req.user)?true:false
     });
 });
 app.get("/about",(req,res)=>{
     res.render("about_us.ejs",{
-        login:login.bool_login
+        login:(req.user)?true:false
     });
 });
 app.get("/profile",(req,res)=>{
     res.render("profile.ejs",{
-        name:user.name,
-        login:user.name
+        name:req.user.name,
+        login:(req.user)?true:false,
     });
 })
+passport.serializeUser((user,cb)=>{
+cb(null,user);}
+)
+passport.deserializeUser((user,cb)=>{
+    cb(null,user);
+})
+
 app.listen(port,(err)=>{
     if (err) throw err;
     console.log(`server is running perfectly on port ${port}`);
